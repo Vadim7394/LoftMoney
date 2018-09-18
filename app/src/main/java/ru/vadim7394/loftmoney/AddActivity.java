2fba62d58101f813e85515d22485edf60a09c651
Вадим Хомяков <vadim7394@gmail.com>
2018-09-19 00:42:06 +0400

    add fab and add items

--- app/src/main/java/ru/vadim7394/loftmoney/AddActivity.java
+++ app/src/main/java/ru/vadim7394/loftmoney/AddActivity.java
@@ -1,14 +1,19 @@
 package ru.vadim7394.loftmoney;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class AddActivity extends AppCompatActivity {
+
+    public static final String KEY_TYPE = "type";
+    public static final String KEY_ITEM = "item";
 
     private EditText nameInput;
     private EditText priceInput;
@@ -36,11 +41,30 @@ public class AddActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
 
+        final String type = getIntent().getExtras().getString(KEY_TYPE);
+
         nameInput = findViewById(R.id.name);
         priceInput = findViewById(R.id.price);
         addBtn = findViewById(R.id.add_btn);
 
         nameInput.addTextChangedListener(generalTextWatcher);
         priceInput.addTextChangedListener(generalTextWatcher);
+
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = nameInput.getText().toString();
+                String price = priceInput.getText().toString();
+
+                Item item = new Item(name, Integer.parseInt(price), type);
+
+                Intent intent = new Intent();
+                intent.putExtra(KEY_ITEM, item);
+
+                setResult(RESULT_OK, intent);
+                finish();
+
+            }
+        });
     }
 }
